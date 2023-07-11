0bffbd6d7bc355df3362a4ec0c3feaa1d7b30df6
joseluismunozzuta <joseluismunozzuta@gmail.com>
2023-07-11 11:18:46 -0500

    last change

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <h1 className='text-black text-3xl text-center py-10 px-4 font-bold'>Joyeaux anniversaire, ma belle princesse🎂🤍</h1>
             <div
                 className="block lg:hidden rounded-full shadow-xl mx-auto h-72 w-72 my-16 bg-cover bg-center heartbeat"
-                style={{ backgroundImage: `url('https://firebasestorage.googleapis.com/v0/b/photoswithandre.appspot.com/o/Imagen%20de%20WhatsApp%202023-07-08%20a%20las%2015.21.46.jpg?alt=media&token=b56747f8-a560-420c-8786-2aacbd10841c')` }}></div>
+                style={{ backgroundImage: `url('/authorPhoto.jpg')` }}></div>
             <h1 className='text-black text-2xl text-center px-2 py-10 font-bold'>Te desea el Dientesitos 🤍</h1>
 
             <Link to="component2" smooth={true} duration={500} className='absolute bottom-0 left-0 p-4'>
